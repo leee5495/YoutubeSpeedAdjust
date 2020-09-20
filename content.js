b276c4f0f6a5b9a4d8bba32fdeef0b03ab0e2a47
Eunjin Lee <leee5495@gmail.com>
2020-09-20 20:19:34 +0900

    add content.js and images

---
+++ content.js
@@ -0,0 +1,26 @@
+window.onload = function temp() {
+	console.log('document loaded'); 
+	var video = document.getElementsByTagName("video")[0];
+	console.log(video);
+
+	var isVerifiedArtist = null;
+	var ChannelName = null;
+
+	while(!isVerifiedArtist && !ChannelName) {
+		isVerifiedArtist = document.querySelector("#upload-info > #channel-name > ytd-badge-supported-renderer > .badge.badge-style-type-verified-artist");
+		console.log(isVerifiedArtist);
+		ChannelName = document.querySelector("#text > a")
+		console.log(ChannelName);
+	}
+	ChannelName = ChannelName.textContent;
+	console.log(ChannelName);
+
+	if (isVerifiedArtist) {
+		// set playback speed to 1.0
+		video.playbackRate = 1.0;
+	}
+	else if (ChannelName.endsWith("- Topic")) {
+		// set playback speed to 1.0
+		video.playbackRate = 1.0;
+	}
+}
