f9edb47bd516d6d63b23f014f91a19a119e7b20b
Eunjin Lee <leee5495@gmail.com>
2020-09-21 01:41:58 +0900

    fix bug

--- content.js
+++ content.js
@@ -1,13 +1,23 @@
 // callback executed when canvas was found
-function handleCanvas(video, ChannelName) {
+function handleCanvas(video, info) {
+	var video = document.getElementsByTagName("video")[0];
 	var isVerifiedArtist = document.querySelector("#upload-info > #channel-name > ytd-badge-supported-renderer > .badge.badge-style-type-verified-artist");
+	var info = document.getElementById('scriptTag');
+	
+	var json = JSON.parse(info.textContent);
+	var channelName = json['author'];
+	var genre = json['genre'];
 	
 	// if playing music
 	if (isVerifiedArtist) {
 		// set playback speed to 1.0
 		video.playbackRate = 1.0;
 	}
-	else if (ChannelName.endsWith("- Topic")) {
+	else if (channelName.endsWith("Topic")) {
+		// set playback speed to 1.0
+		video.playbackRate = 1.0;
+	}
+	else if (genre == 'Music') {
 		// set playback speed to 1.0
 		video.playbackRate = 1.0;
 	}
@@ -18,9 +28,9 @@ var observer = new MutationObserver(function (mutations, me) {
   // `mutations` is an array of mutations that occurred
   // `me` is the MutationObserver instance
   var video = document.getElementsByTagName("video")[0];
-  var ChannelName = document.querySelector("#text > a");
-  if (video && ChannelName) {
-    handleCanvas(video, ChannelName.textContent);
+  var info = document.getElementById('scriptTag');
+  if (video && info) {
+    handleCanvas(video, info.textContent);
     //me.disconnect(); // stop observing
     return;
   }
